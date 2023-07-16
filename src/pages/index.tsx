58314f58b271a2e30ea7a2e026b872d35ae3577e
Gideon Etim <gidiblack@gmail.com>
2023-07-17 00:37:42 +0100

    clients section with carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import ContactTabs from "@/components/ContactTabs";
 import HomeHero from "@/components/HomeHero";
 import OptimizedImage from "@/components/OptimizedImage";
 import PageSection from "@/components/PageSection";
-import { Grid, GridItem, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Text } from "@chakra-ui/react";
 import Punches from "@/assets/640-ball-bearing-stainless-metal-roller-for-machine-industrial-angular-contact-isolated-on-white-background.jpg";
 import Wheels from "@/assets/metal-preparations-are-ready-following-stage-processing.jpg";
 import Knives from "@/assets/high-angle-medical-scalpel-blades.jpg";
@@ -11,6 +11,19 @@ import Gears from "@/assets/gears-table.jpg";
 import ToolCabinet from "@/assets/view-toolbox-machines.jpg";
 import CastIron from "@/assets/cast-iron-vs-steel.webp";
 import IntricateParts from "@/assets/various-work-tools-worktop.jpg";
+import Unilever from "@/assets/unilever-logo.png";
+import Twinings from "@/assets/Twinings.png";
+import Nestle from "@/assets/nestle-logo.png";
+import Gsk from "@/assets/gsk-logo.jpg";
+import SweetNutrition from "@/assets/sweet-nutrition.png";
+import Perfetti from "@/assets/perfetti.png";
+import Suntory from "@/assets/suntory.png";
+import MrChef from "@/assets/mrchef.png";
+import GbFoods from "@/assets/GB-FOODS.png";
+import ApFoods from "@/assets/APFoods.png";
+import Bayswater from "@/assets/bayswater.fw_.png";
+import ReckittBenckiser from "@/assets/ReckittBenckiser.fw_.png";
+import Slider from "react-slick";
 
 export default function Home() {
   const products = [
@@ -47,6 +60,96 @@ export default function Home() {
       img: IntricateParts.src,
     },
   ];
+
+  const clients = [
+    {
+      name: "Unilever logo",
+      img: Unilever.src,
+    },
+    {
+      name: "Twinings logo",
+      img: Twinings.src,
+    },
+    {
+      name: "Nestle",
+      img: Nestle.src,
+    },
+    {
+      name: "Gsk",
+      img: Gsk.src,
+    },
+    {
+      name: "Sweet Nutrition",
+      img: SweetNutrition.src,
+    },
+    {
+      name: "Perfetti",
+      img: Perfetti.src,
+    },
+    {
+      name: "Suntory logo",
+      img: Suntory.src,
+    },
+    {
+      name: "Mr Chef logo",
+      img: MrChef.src,
+    },
+    {
+      name: "GB Foods",
+      img: GbFoods.src,
+    },
+    {
+      name: "AB Foods",
+      img: ApFoods.src,
+    },
+    {
+      name: "Bayswater",
+      img: Bayswater.src,
+    },
+    {
+      name: "ReckittBenckiser",
+      img: ReckittBenckiser.src,
+    },
+  ];
+
+  // responsive breakpoints are "max-width"
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 5000,
+    autoplaySpeed: 0,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    cssEase: "linear",
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 1240,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 765,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 448,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  };
 
   return (
     <>
@@ -121,6 +224,42 @@ export default function Home() {
             </GridItem>
           ))}
         </Grid>
+      </PageSection>
+      <PageSection>
+        <Text as={"h2"} textAlign={"center"} mb={2}>
+          CLIENTS
+        </Text>
+        <Text textAlign={"center"} fontSize={20} color={"dark.500"}>
+          Our clients are our most priced assets. They have been the engine
+          driving our innovations in almost 30 years of existence
+        </Text>
+        <Box
+          mt={[6, null, 8, 10, 12]}
+          mx={[
+            "-1rem",
+            "-1.5rem",
+            "-2rem",
+            "-4rem",
+            "-5rem",
+            "-6.25rem",
+            "-8rem",
+          ]}
+        >
+          <Slider {...settings}>
+            {clients.map(({ img, name }, i) => (
+              <div key={i}>
+                <OptimizedImage
+                  src={img}
+                  w={{ base: "9rem", md: "10rem", lg: "11.25rem" }}
+                  h={{ base: "5rem", md: "5.5rem", lg: "6.25rem" }}
+                  borderRadius={"8px"}
+                  mx={"auto"}
+                  alt={name}
+                />
+              </div>
+            ))}
+          </Slider>
+        </Box>
       </PageSection>
     </>
   );
