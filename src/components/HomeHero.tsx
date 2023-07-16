763089a282afe0502ae801f7e02830e9f8f63620
Gideon Etim <gidiblack@gmail.com>
2023-07-16 16:08:36 +0100

    home hero section

---
+++ src/components/HomeHero.tsx
@@ -0,0 +1,195 @@
+import {
+  Box,
+  BoxProps,
+  Flex,
+  HStack,
+  Heading,
+  Image,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import routes from "./routes";
+import NextLink from "next/link";
+
+function HomeHero() {
+  const arrowStyles: BoxProps = {
+    cursor: "pointer",
+    pos: "absolute",
+    top: "50%",
+    w: "auto",
+    transform: "translateY(-50%)",
+    py: "6px",
+    px: "12px",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "24px",
+    transition: "0.6s ease",
+    borderRadius: 4,
+    userSelect: "none",
+    _hover: {
+      opacity: 0.8,
+      bg: "brand.300",
+    },
+  };
+  const slides = [
+    {
+      img: "https://images.pexels.com/photos/2599537/pexels-photo-2599537.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      label: "Production of Intricate Industrial Parts",
+      description: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+      img: "https://images.pexels.com/photos/2714581/pexels-photo-2714581.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      label: "Second Slide",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      img: "https://images.pexels.com/photos/2878019/pexels-photo-2878019.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+      label: "Third Slide",
+      description:
+        "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+    },
+    {
+      img: "https://images.pexels.com/photos/1142950/pexels-photo-1142950.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      label: "Fourth Slide",
+      description: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+      img: "https://images.pexels.com/photos/3124111/pexels-photo-3124111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      label: "Fifth Slide",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+  ];
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slidesCount = slides.length;
+
+  const prevSlide = () => {
+    setCurrentSlide((s) => (s === 0 ? slidesCount - 1 : s - 1));
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide((s) => (s === slidesCount - 1 ? 0 : s + 1));
+  };
+
+  const setSlide = (slide: number) => {
+    setCurrentSlide(slide);
+  };
+
+  const carouselStyle = {
+    transition: "all .5s",
+    ml: `-${currentSlide * 100}%`,
+  };
+
+  useEffect(() => {
+    const id = setInterval(
+      () => setCurrentSlide((s) => (s === slidesCount - 1 ? 0 : s + 1)),
+      5000
+    );
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [slidesCount]);
+
+  return (
+    <Box
+      as={"section"}
+      textAlign={"center"}
+      bg={"linear-gradient(180deg, #DDE3F0 0%, rgba(193, 211, 255, 0.38) 100%)"}
+      px={"1rem"}
+      py={"3.5rem"}
+      pos={"relative"}
+    >
+      <Heading as={"h1"} letterSpacing={"2px"} mb={8} fontWeight={"extrabold"}>
+        EMMA-TOB Technologies Ltd.
+      </Heading>
+      <Flex w="full" pos="relative" overflow="hidden">
+        <Flex h="37rem" w="full" {...carouselStyle}>
+          {slides.map((slide, sid) => (
+            <Box key={`slide-${sid}`} boxSize="full" shadow="md" flex="none">
+              <Image
+                src={slide.img}
+                alt="carousel image"
+                boxSize="full"
+                backgroundSize="cover"
+                borderRadius={12}
+              />
+              <Box
+                bg={{
+                  base: "linear-gradient(180deg, #000 17.25%, rgba(0, 0, 0, 0.00) 20.45%)",
+                  md: "linear-gradient(195deg, #000 20.75%, rgba(0, 0, 0, 0.00) 30.55%)",
+                  lg: "linear-gradient(193deg, #000 18.75%, rgba(0, 0, 0, 0.00) 32.65%)",
+                }}
+                pos="absolute"
+                borderRadius={12}
+                boxSize={"full"}
+                top={0}
+              >
+                <Stack
+                  p={6}
+                  bottom="24px"
+                  textAlign={{ base: "center", md: "right" }}
+                  w="full"
+                  spacing={3}
+                  color="gray.50"
+                  ml={"auto"}
+                  maxW={{ md: "18rem", lg: "20.5rem" }}
+                >
+                  <Text
+                    as={"h3"}
+                    fontSize={[20, null, 22, 24]}
+                    fontFamily={"Lato"}
+                    fontWeight={"semibold"}
+                  >
+                    {slide.label}
+                  </Text>
+                  <Link
+                    as={NextLink}
+                    fontSize={[14, null, 15, 16]}
+                    fontStyle={"italic"}
+                    textDecor={"underline"}
+                    href={routes.services}
+                    _hover={{
+                      color: "brand.400",
+                    }}
+                  >
+                    Learn more
+                  </Link>
+                </Stack>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+        <Text {...arrowStyles} left="0" onClick={prevSlide}>
+          &#10094;
+        </Text>
+        <Text {...arrowStyles} right="0" onClick={nextSlide}>
+          &#10095;
+        </Text>
+      </Flex>
+      <HStack justify="center" mt={6} w="full">
+        {Array.from({
+          length: slidesCount,
+        }).map((_, slide) => (
+          <Box
+            key={`dots-${slide}`}
+            cursor="pointer"
+            boxSize={["12px", null, "16px"]}
+            m="0 2px"
+            bg={currentSlide === slide ? "brand.500" : "gray.300"}
+            rounded="50%"
+            display="inline-block"
+            transition="background-color 0.6s ease"
+            _hover={{
+              bg: "brand.500",
+            }}
+            onClick={() => setSlide(slide)}
+          ></Box>
+        ))}
+      </HStack>
+    </Box>
+  );
+}
+
+export default HomeHero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,16 @@
-import PageSection from "@/components/PageSection";
-import { Heading, Text } from "@chakra-ui/react";
+import HomeHero from "@/components/HomeHero";
 
 export default function Home() {
   return (
-    <PageSection textAlign={"center"}>
-      <Heading as={"h1"} my={10} fontWeight={"extrabold"}>
-        EMMA-TOB
-      </Heading>
-      <Text w={"50%"} mx={"auto"} fontSize={18} fontStyle={"italic"}>
+    <>
+      <HomeHero />
+      {/* <Text w={"50%"} mx={"auto"} fontSize={18} fontStyle={"italic"}>
         Emma-Tob is your trusted partner for designing, fabricating and
         supplying machines including rare and hard-to-find spare parts. With our
         extensive experience and expertise, we help our customers significantly
         reduce their production costs, giving them a competitive edge in the
         market.
-      </Text>
-    </PageSection>
+      </Text> */}
+    </>
   );
 }
